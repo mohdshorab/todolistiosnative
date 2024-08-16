7b238a7240bd8523ca48ef896b1346540a1f2b53
mohdshorab <mohdshorab474@gmail.com>
2024-08-17 00:18:48 +0530

    Delete Button working, showing alert, two sections for pending and completed

--- To-Do List/Controllers/TaskDetailsViewController.swift
+++ To-Do List/Controllers/TaskDetailsViewController.swift
@@ -8,12 +8,131 @@
 import UIKit
 
 class TaskDetailsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if task == nil {
+            taskTitle.placeholder = "Enter the title"
+            taskDesc.text = "Enter here"
+        }
+        else {
+            taskTitle.text = task?.title
+            taskDesc.text = task?.taskDesc
+        }
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    var task : TaskEntity?
+    
+    @IBOutlet weak var taskTitle: UITextField!
+    
+    @IBOutlet weak var taskDesc: UITextView!
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBAction func saveData(_ sender: Any) {
+        
+        if (taskTitle.text == "" && taskDesc.text == "" || taskDesc.text == "Enter here"){
+            let alertController = UIAlertController(title: "Empty Details", message: "Task details cant be empty.", preferredStyle: .alert)
+            let alertForOk = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(alertForOk)
+            present(alertController, animated: true, completion: nil)
+        }
+        
+        else {
+            
+//    checking if we are getting data from previous screen, like if user click on task we using this same
+            
+            if task == nil {
+                
+                if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
+                    let task = TaskEntity( context: context)
+                    task.title = taskTitle.text
+                    task.taskDesc = taskDesc.text
+                    task.isCompleted = false
+                    
+                    (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                    navigationController?.popViewController(animated: true)
+                    print("Task Saved")
+                }
+            }
+            
+//            no changes so do nothing
+            else {
+                
+                if taskTitle.text == task?.title && taskDesc.text == task?.taskDesc {
+                    return
+                } else {
+                    
+//Changes done => updating the same task => nav to previous screen
+                    if ((UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext) != nil {
+                        // Update the existing entry instead of creating a new one
+                        task?.title = taskTitle.text
+                        task?.taskDesc = taskDesc.text
+                        task?.isCompleted = false
+                        
+                        // Save the changes
+                        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                        navigationController?.popViewController(animated: true)
+                        print("Task Updated")
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
+    @IBAction func deleteData(_ sender: Any) {
+        
+        let buttonText = self.deleteButton.titleLabel?.text ?? ""
+        var isDelete: Bool
+        if buttonText == "Delete" {
+            isDelete = true
+        } else {
+            isDelete = false
+        }
+        
+        let title = isDelete ? "Confirm Deletion" : "Confirm Cancellation"
+        let message = isDelete ? "Are you sure you want to delete?" : "Are you sure you want to cancel?"
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        
+        let actionForYes = UIAlertAction(title: "Yes", style: .destructive) { _ in
+            if isDelete {
+                if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                    if let taskToBeDeleted = self.task {
+                        context.delete(taskToBeDeleted)
+                    }
+                    try? context.save()
+                }
+            }
+            self.navigationController?.popViewController(animated: true)
+        }
+        
+        let actionForNo = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        
+        alertController.addAction(actionForYes)
+        alertController.addAction(actionForNo)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if task == nil {
+            self.deleteButton.setTitle("Cancel", for: .normal)
+        } else {
+            self.deleteButton.setTitle("Delete", for: .normal)
+        }
+    }
+    
+
+    
     
 
     /*
--- To-Do List/Controllers/TaskListTableTableViewController.swift
+++ To-Do List/Controllers/TaskListTableTableViewController.swift
@@ -8,82 +8,68 @@
 import UIKit
 
 class TaskListTableTableViewController: UITableViewController {
+    
+    var tasks : [TaskEntity] = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
+            if let entriesFromCoreEntity = try? context.fetch(TaskEntity.fetchRequest())
+                as? [TaskEntity]{
+                print("Fetching tasks from core data")
+                tasks = entriesFromCoreEntity
+                tableView.reloadData()
+                
+            }
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "Pending" : "Completed"
+    }
+
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let filteredTasks = tasks.filter { $0.isCompleted == (section == 1) }
+        return filteredTasks.count
     }
-
-    /*
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskListCell", for: indexPath)
+        let filteredTasks = tasks.filter { $0.isCompleted == (indexPath.section == 1) }
+                let task = filteredTasks[indexPath.row]
+                
+                // Configure the cell
+                cell.textLabel?.text = task.title ?? "Unable to fetch"
+                return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let task = tasks[indexPath.row]
+        performSegue(withIdentifier: "moveToDetails", sender: task)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let taskDetailViewController = segue.destination as? TaskDetailsViewController {
+            if let taskGoingToBeSent = sender as? TaskEntity{
+                taskDetailViewController.task = taskGoingToBeSent
+            }
+        }
     }
-    */
 
 }
